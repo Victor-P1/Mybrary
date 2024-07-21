2fc1701e1dcd6a264ab36e8ec9ad6d75a00090c6
Victor-P1 <162100289+Victor-P1@users.noreply.github.com>
2024-07-21 19:49:23 +0100

    Initial Author

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
-
 const express = require('express');
 const Mongoose = require('mongoose')
 const path = require('path');
-const app = express();
-const indexRouter = require('./routes/index'); // Ensure the path is correct
+const expressLayouts = require('express-ejs-layouts');
+const app = express()
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bodyParser = require('body-parser')
+
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views')); // Set the views directory
+app.set('layout', 'layouts/layout')
 
-// Use the index router
+app.use(expressLayouts)
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+// Use the router
 app.use('/', indexRouter);
+app.use('/authors', authorRouter)
 
 const mongoose = Mongoose.connect(process.env.DATABASE_URL)
   .then(() => console.log('MongoDB connected'))
